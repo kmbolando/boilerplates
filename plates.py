ca42524cdce5b8873ca14c2a8dabd4f16d2dbc5d
Kenneth B <kmbolando@eths202.org>
2018-03-13 21:41:57 -0500

    Create plates.py

---
+++ plates.py
@@ -0,0 +1,8 @@
+#unlicense.org
+#in case importing the numpy/scipy libraries should be avoided:
+
+def zeros(item, quanity):
+	return [item] * quanity
+
+def simple_matrix(item,quanity):
+    return [item] * quanity
